8301a7a34084875150e70570b851831139862efb
warlospp <warlospp@hotmail.com>
2026-08-21 00:22:09 -0500

    Igualacion de estructuras

--- 5.-Queries/3.-DDM.sql
+++ 5.-Queries/3.-DDM.sql
@@ -3,21 +3,21 @@
 #######
 
 # Activar
-ALTER TABLE dbo.Clients
-ALTER COLUMN IdentificationNumber
+ALTER TABLE dbo.clients
+ALTER COLUMN identification_number
 ADD MASKED WITH (FUNCTION = 'default()');
 
-ALTER TABLE dbo.Clients
-ALTER COLUMN Email
+ALTER TABLE dbo.clients
+ALTER COLUMN email
 ADD MASKED WITH (FUNCTION = 'partial(2,"XXXXXXX",2)');
 
 # Desactivar
-ALTER TABLE dbo.Clients
-ALTER COLUMN IdentificationNumber
+ALTER TABLE dbo.clients
+ALTER COLUMN identification_number
 DROP MASKED;
 GO
 
-ALTER TABLE dbo.Clients
-ALTER COLUMN Email
+ALTER TABLE dbo.clients
+ALTER COLUMN email
 DROP MASKED;
 GO

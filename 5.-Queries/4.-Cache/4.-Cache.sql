3f9e5ccd1b98a15d27e363e3bb880fa3ba003de2
warlospp <warlospp@hotmail.com>
2026-08-22 12:15:35 -0500

    Mejora en queries

--- 5.-Queries/4.-Cache/4.-Cache.sql
+++ 5.-Queries/4.-Cache/4.-Cache.sql
@@ -6,5 +6,10 @@ http://localhost:5000/api/clients?$select=client_id,first_name,last_name
 ---------------
 -- Modificar --
 ---------------
+-- MSSQL
 UPDATE dbo.clients
+SET first_name = 'SSSSSSSSSSSSSS'
+
+--PostgreSQL
+UPDATE clients
 SET first_name = 'SSSSSSSSSSSSSS'

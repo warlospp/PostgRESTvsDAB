00c53ce0df47743e252c191da89181d14f67cc8d
warlospp <warlospp@hotmail.com>
2026-08-22 12:42:50 -0500

    Mejora de Sentencias

--- 5.-Queries/4.-Cache/4.-Cache.sql
+++ 5.-Queries/4.-Cache/4.-Cache.sql
@@ -7,9 +7,21 @@ http://localhost:5000/api/clients?$select=client_id,first_name,last_name
 -- Modificar --
 ---------------
 -- MSSQL
+--Levantar Profiler
 UPDATE dbo.clients
 SET first_name = 'SSSSSSSSSSSSSS'
 
 --PostgreSQL
+SELECT pg_stat_statements_reset();
+
 UPDATE clients
 SET first_name = 'SSSSSSSSSSSSSS'
+
+SELECT
+    calls,
+    total_exec_time,
+    rows,
+    query
+FROM pg_stat_statements
+WHERE query ILIKE '%clients%'
+ORDER BY calls DESC;
